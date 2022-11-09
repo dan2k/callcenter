e05a8b8b211154a6089edd8071eb181812698867
dan2k <sangtong@gmail.com>
2022-11-09 14:05:03 +0700

    finish  show picturename and upload picture more

--- src/logics/followup.js
+++ src/logics/followup.js
@@ -2,18 +2,23 @@ import {api,start,close,errAlert} from '@/helpers'
 import {ref} from "vue"
 import { useAuthStore } from '@/store';
 import { useRouter } from 'vue-router';
+import { useUpload } from "@/plugins/fileupload";
 
 export const useFollowup=()=>{
     const detail=ref({})
     const store = useAuthStore()
     const router=useRouter()
     const jobs =ref([])
+    const pics=ref([])
     const solve=ref(null)
+    const {files}=useUpload()
+    files.value.length=0
     const getDetail=async (jobid)=>{
         start()
         try{
             let rs=await api.get(`callcenter/job/v1/getJobDetail/${jobid}`)
             detail.value=rs.data.data[0]
+            pics.value=await getPic(jobid)
             if(detail.value.job_status==1){
                 await getSolve(jobid)
             }
@@ -22,6 +27,18 @@ export const useFollowup=()=>{
         }
         close()
     }
+    const getPic=async (jobid)=>{
+        start()
+        try{
+            let rs=await api.get(`callcenter/job/v1/getJobPic/${jobid}`)
+            close()
+            return rs.data.data
+        }catch(err){
+            errAlert(err)
+        }
+        close()
+    }
+
     const getSolve=async (jobid)=>{
         start()
         try{
@@ -45,12 +62,36 @@ export const useFollowup=()=>{
     const showDetail=(jobid)=>{
         router.replace({path:`/followup/${jobid}`})
     }
+    const savePic =async (jobid) => {
+            const formData=new FormData();
+            //clear null objects
+            files.value=files.value.filter((ob,i)=>ob.url)
+            for(let i=0;i<files.value.length;i++){
+                formData.append('file_'+i,files.value[i].obj.files[0]);
+            }
+            start()
+            try{
+                await api.post(`callcenter/job/v1/updatePic/${jobid}`, formData, {
+                    headers: {
+                    "Content-Type": "multipart/form-data",
+                    },
+                });
+                
+            }catch(err){
+                errAlert(err)
+            }
+            close()
+    };
     return {
         detail,
         jobs,
         solve,
+        pics,
+        files,
         getDetail,
         showDetail,
         getJobs,
+        savePic,
+        getPic,
     }
 }
