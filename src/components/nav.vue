cd165525eb829725c6c793c0491acf3e4d58113a
dan2k <sangtong@gmail.com>
2023-08-09 10:25:59 +0700

    change bgcolor and color

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-info bg-opacity-25" style="font-weight: bolder;font-size:30px;">
+    <nav class="navbar navbar-expand-lg " style="font-weight: bolder;font-size:30px;background-color:#16351a;color:#bbe9f9;">
       <img src="/CDG.png" style="width:25%;left:-10vw;" class="position-absolute  mx-3" alt="">
       <div class="container mx-auto"><span class="mx-auto mt-2">ระบบรับแจ้งปัญหา</span></div>
       <span class="position-absolute " style="cursor:pointer;right:10px;" @click="logout"><i class="fas fa-sign-out-alt  text-danger" ></i></span>
