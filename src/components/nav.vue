bf67ea3e67f769b9cceaa510bb34310f589a393d
dan2k <sangtong@gmail.com>
2023-08-09 09:58:33 +0700

    change logo

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-info bg-opacity-25" style="font-weight: bolder;font-size:30px;">
-      <img src="/CDG.jpg" style="width:5%" class="position-absolute mx-3" alt="">
+      <img src="/CDG.png" style="width:25%;left:-10vw;" class="position-absolute  mx-3" alt="">
       <div class="container mx-auto"><span class="mx-auto mt-2">ระบบรับแจ้งปัญหา</span></div>
       <span class="position-absolute " style="cursor:pointer;right:10px;" @click="logout"><i class="fas fa-sign-out-alt  text-danger" ></i></span>
     </nav>
