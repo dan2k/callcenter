87f996cb52a4dc877748d631e26249e5a4285ac6
Seangtong Huankhai <seangtong.h@cdg.co.th>
2024-05-14 14:31:37 +0700

    hi image when no picture

--- src/views/search/search-detail.vue
+++ src/views/search/search-detail.vue
@@ -38,7 +38,7 @@
                     </div>
                     
                 </div>
-                <div class="row mx-2" v-if="isPic==='1'">
+                <div class="row mx-2" v-if="isPic==='1' && pics.lenght">
                     <div class="col-12">
                         <h6 class="card-title">รูปภาพ:</h6>
                         
